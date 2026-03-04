1a6a03c78d50ca482a563569495f7c62c5822e3c
Bruno <innocentea.99@gmail.com>
2026-03-04 14:59:20 +0100

    fix seriale.c

--- seriale.c
+++ seriale.c
@@ -7,13 +7,14 @@
 
 bool gaussianElimination(int n, int k, bool **matrix) {
     int rank = 0;
+    
     bool solution[k-1];
     for (int i = 0; i < k; i++) {
-            solution[i] = false;
+    solution[i] = false;
     }
 
 
-    for (int col = 0; col < k && rank < n; col++) {
+    for (int col = 0; col < k-1 && rank < n; col++) { //scorre per colonna per cercare il pivot
         // Trova il pivot
         int pivot = -1;
         for (int row = rank; row < n; row++) {
@@ -27,7 +28,7 @@ bool gaussianElimination(int n, int k, bool **matrix) {
 
         // Scambia la riga del pivot con la riga corrente
         if (pivot != rank) {
-            for (int j = 0; j <= k; j++) {
+            for (int j = 0; j < k; j++) {  //!!!! <=
                 bool temp = matrix[rank][j];
                 matrix[rank][j] = matrix[pivot][j];
                 matrix[pivot][j] = temp;
@@ -38,7 +39,7 @@ bool gaussianElimination(int n, int k, bool **matrix) {
         // Elimina la colonna sotto il pivot
         for (int row = rank + 1; row < n; row++) {
             if (matrix[row][col]) {
-                for (int j = col; j <= k; j++) {
+                for (int j = col; j < k; j++) { //!!!!<=
                     matrix[row][j] ^= matrix[rank][j]; //XOR tra pivot e colonna da eliminare, dato che le variabili sono booleane fare xor elemento per elemento corrisponde ad un'eliminazione
                 }
 
@@ -48,26 +49,32 @@ bool gaussianElimination(int n, int k, bool **matrix) {
         rank++;
         
     }
-
+    
 
     // Controlla se il sistema è risolvibile
     for (int row = rank; row < n; row++) {
-        if (matrix[row][k]) {
-            printf("Sistema irrisolvibile.\n");
+
+        if (matrix[row][k-1]) {
+            
+            
             return false;
         }
     }
 
     // Back substitution
+
+
+
     for (int i = rank - 1; i >= 0; i--) {
-        solution[i] = matrix[i][k];
+        
+        solution[i] = matrix[i][k-1];
      
         for (int j = i + 1; j < k; j++) { // scorro tutte le variabili già risolte moltiplico (AND) il valore con il coefficiente 
             solution[i] ^= (matrix[i][j] && solution[j]); // é come spostare variabili a dx dell'equazione somma o sottrazione è sempre XOR
+            
         }
+
     }
 
-    //printSolution(solution, n, k);
     return true;
 }
-
