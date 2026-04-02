02b7f8779e8ba717e4eac50394b1204649c8b6bd
Bruno <innocentea.99@gmail.com>
2026-04-02 09:53:50 +0200

    Add tests theta 0.3

--- Analysis.R
+++ Analysis.R
@@ -10,24 +10,24 @@ data_ser <-data_ser %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
-data_p1 <-read.csv("result_data/risultati_p1_01_20_3050.csv")
+data_p1 <-read.csv("result_data/risultati_p1_01_20_3060.csv")
 
 data_p1 <-data_p1 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
 
-data_p2 <-read.csv("result_data/risultati_p2_01_20_3050.csv")
+data_p2 <-read.csv("result_data/risultati_p2_01_20_3060.csv")
 data_p2 <-data_p2 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
-data_p3 <-read.csv("result_data/risultati_p3_01_20_3050.csv")
+data_p3 <-read.csv("result_data/risultati_p3_01_20_3060.csv")
 data_p3 <-data_p3 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
-data_p4 <-read.csv("result_data/risultati_p4_01_20_3050.csv")
+data_p4 <-read.csv("result_data/risultati_p4_01_20_3060.csv")
 data_p4 <-data_p4 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
@@ -44,7 +44,7 @@ data_p3  <- data_p3 %>% mutate(tipo = "p3")
 data_p4  <- data_p4 %>% mutate(tipo = "p4")
 data_p5  <- data_p5 %>% mutate(tipo = "p5")
 # Unisci i dataset
-data_all <- bind_rows(data_ser, data_p3 , data_p4,data_p5)
+data_all <- bind_rows(data_ser, data_p1, data_p2, data_p3 , data_p4)
 
 
 
@@ -56,13 +56,13 @@ ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
     x = "n^2",
     y = "Tempo medio di esecuzione",
     color = "Tipo esecuzione",
-    title = "Confronto tempi seriale vs p1 vs p2 vs p3 vs p5;\n theta =0.1"
+    title = "Confronto tempi seriale vs p1 vs p2 vs p3 vs p4;\n theta =0.1"
   ) +
   theme_minimal(base_size = 14) # tema pulito
 
 
 #secondo plot
-data_all <- bind_rows(data_p3, data_p4, data_p5)
+data_all <- bind_rows(data_p3, data_p4)
 ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
   geom_line(size = 1) +       
   #geom_point() +                
@@ -70,7 +70,7 @@ ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
     x = "n^2",
     y = "Tempo medio di esecuzione",
     color = "Tipo esecuzione",
-    title = "Confronto tempi p3 vs p5; theta =0.1"
+    title = "Confronto tempi p3 vs p4; theta =0.1"
   ) +
   theme_minimal(base_size = 14) # tema pulito
 
@@ -80,10 +80,10 @@ ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
 
 data_ser <- read.csv("result_data/risultati_seriale_01_20_3060.csv")
 
-data_p1 <-read.csv("result_data/risultati_p1_01_20_3050.csv")
-data_p2 <-read.csv("result_data/risultati_p2_01_20_3050.csv")
-data_p3 <-read.csv("result_data/risultati_p3_01_20_3050.csv")
-data_p4 <-read.csv("result_data/risultati_p4_01_20_3050.csv")
+data_p1 <-read.csv("result_data/risultati_p1_01_20_3060.csv")
+data_p2 <-read.csv("result_data/risultati_p2_01_20_3060.csv")
+data_p3 <-read.csv("result_data/risultati_p3_01_20_3060.csv")
+data_p4 <-read.csv("result_data/risultati_p4_01_20_3060.csv")
 data_p5 <-read.csv("result_data/risultati_p5_01_20_3050.csv")
 
 identical(data_p1$result, data_ser$result)
@@ -99,28 +99,45 @@ identical(data_p5$result, data_p3$result)
 ####03#######################################
 
 
-data_ser <- read.csv("result_data/risultati_seriale_03.csv")
+data_ser <- read.csv("result_data/risultati_seriale_03_20_3060.csv")
+data_p1 <-read.csv("result_data/risultati_p1_03_20_3060.csv")
+data_p2 <-read.csv("result_data/risultati_p2_03_20_3060.csv")
+data_p3 <-read.csv("result_data/risultati_p3_03_20_3060.csv")
+data_p4 <-read.csv("result_data/risultati_p4_03_20_3060.csv")
+
+identical(data_p1$result, data_ser$result)
+identical(data_p2$result, data_ser$result)
+identical(data_p3$result, data_ser$result)
+identical(data_p4$result, data_ser$result)
+identical(data_p5$result, data_ser$result)
+
+
+
+data_ser <- read.csv("result_data/risultati_seriale_03_20_3060.csv")
 
 data_ser <-data_ser %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
-data_p1 <-read.csv("result_data/risultati_p1_03.csv")
+data_p1 <-read.csv("result_data/risultati_p1_03_20_3060.csv")
 
 data_p1 <-data_p1 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
 
-plot(data_ser$n^2, data_ser$media, type="l")
-
-data_p2 <-read.csv("result_data/risultati_p2_03.csv")
+data_p2 <-read.csv("result_data/risultati_p2_03_20_3060.csv")
 data_p2 <-data_p2 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
-data_p3 <-read.csv("result_data/risultati_p3_03.csv")
+data_p3 <-read.csv("result_data/risultati_p3_03_20_3060.csv")
 data_p3 <-data_p3 %>%
+  group_by(n) %>%
+  summarise(t_medio = mean(tempo_esecuzione))
+
+data_p4 <-read.csv("result_data/risultati_p4_03_20_3060.csv")
+data_p4 <-data_p4 %>%
   group_by(n) %>%
   summarise(t_medio = mean(tempo_esecuzione))
 
@@ -128,11 +145,9 @@ data_ser <- data_ser %>% mutate(tipo = "seriale")
 data_p1  <- data_p1 %>% mutate(tipo = "p1")
 data_p2  <- data_p2 %>% mutate(tipo = "p2")
 data_p3  <- data_p3 %>% mutate(tipo = "p3")
+data_p4  <- data_p4 %>% mutate(tipo = "p4")
 # Unisci i dataset
-#data_all <- bind_rows(data_ser, data_p1, data_p2, data_p3)
-data_all <- bind_rows(data_p2, data_p3)
-
-
+data_all <- bind_rows(data_ser, data_p1, data_p2, data_p3 , data_p4)
 
 ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
   geom_line(size = 1) +       
@@ -141,21 +156,27 @@ ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
     x = "n^2",
     y = "Tempo medio di esecuzione",
     color = "Tipo esecuzione",
-    title = "Confronto tempi seriale vs p1"
+    title = "Confronto tempi seriale vs p1 vs p2 vs p3 vs p4;\n theta =0.3"
   ) +
   theme_minimal(base_size = 14) # tema pulito
 
 
-data_ser <- read.csv("result_data/risultati_seriale_03.csv")
+#secondo plot
+data_all <- bind_rows(data_p3, data_p4)
+ggplot(data_all, aes(x = n^2, y = t_medio, color = tipo)) +
+  geom_line(size = 1) +       
+  #geom_point() +                
+  labs(
+    x = "n^2",
+    y = "Tempo medio di esecuzione",
+    color = "Tipo esecuzione",
+    title = "Confronto tempi p3 vs p4; theta =0.3"
+  ) +
+  theme_minimal(base_size = 14) # tema pulito
 
-data_p1 <-read.csv("result_data/risultati_p1_03.csv")
-data_p2 <-read.csv("result_data/risultati_p2_03.csv")
-data_p3 <-read.csv("result_data/risultati_p3_03.csv")
 
-identical(data_p1$result, data_ser$result)
-identical(data_p2$result, data_ser$result)
-identical(data_p1$result, data_ser$result)
-identical(data_p4$result, data_ser$result)
+
+
 
 
 ### 05 ################
